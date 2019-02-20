5689280e9e4a20e24dbc8fcffa6ad64be012e704
Mushfiqur Rahman <mushfiq.moon013@gmail.com>
2019-02-21 01:03:14 +0600

    style added form materializecss

--- index.php
+++ index.php
@@ -10,35 +10,68 @@
 </head>
 <body>
 
-    <form action="show.php" method="POST">
-        <div class="row">
-            <div class="col s4">
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input type="text" name="user" id="autocomplete-input" class="autocomplete">
-                        <label for="autocomplete-input">NAME</label>
-                    </div>
-                </div>
-            </div>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <!-- <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li> -->
+                <li><a href="collapsible.html">ADMIN</a></li>
+            </ul>
         </div>
-        <div class="row">
-            <div class="col s4">
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input type="password" name="pass" id="autocomplete-input" class="autocomplete">
-                        <label for="autocomplete-input">PASSWORD</label>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </nav>
 
+
+    <div class="container">
         <div class="row">
-            <div class="col s4">
-                <button class="btn waves-effect waves-light" type="submit" name="submit">Submit     
-                </button>
+            <div class="col s6 offset-s3">
+            <div class="card grey lighten-4">
+                <div class="card-content">
+                    <div class="row">
+                        <div class="col s12">
+                            <div class="card-title center-align">
+                                <h5>This should be vertically aligned</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <form action="show.php" method="POST">
+                        <div class="row">
+                            <div class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <input type="text" name="user" id="autocomplete-input" class="autocomplete">
+                                        <label for="autocomplete-input">NAME</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <input type="password" name="pass" id="autocomplete-input-two" class="autocomplete">
+                                        <label for="autocomplete-input-two">PASSWORD</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col s12">
+                                <button class="btn waves-effect waves-light" type="submit" name="submit">Submit     
+                                </button>
+                            </div>
+                        </div>
+                   
+                    </form>
+                </div>
+                
+            </div>
             </div>
         </div>
-    </form>
+    </div>
+
+
 
 
     <!-- Compiled and minified JavaScript -->
